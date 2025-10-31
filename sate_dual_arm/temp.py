6976d166fb4cc6a57396f1498edfe17ade4f2ad6
akiyoshi <uchida.akiyoshi.s3@dc.tohoku.ac.jp>
2025-10-31 15:04:16 +0900

    Update thruster quaternion values in sat_dual_arm model for improved accuracy

--- sate_dual_arm/temp.py
+++ sate_dual_arm/temp.py
@@ -1,11 +1,5 @@
 import numpy as np
 
-Rbm = np.array([[-1, 0, 0],
-                [0, 0, -1],
-                [0, -1, 0]])
-quat_bm = np.array([0.7071, -0.7071, 0, 0])  # w, x, y, z
-quat_0 = np.array([-0.354, -0.146, -0.854, 0.354])  # w, x, y, z
-
 def multiply_quat(q1, q2):
     w1, x1, y1, z1 = q1
     w2, x2, y2, z2 = q2
@@ -15,5 +9,14 @@ def multiply_quat(q1, q2):
     z = w1*z2 + x1*y2 - y1*x2 + z1*w2
     return np.array([w, x, y, z])
 
-quat_thruster = multiply_quat(quat_bm, quat_0)
-print("Quaternion for thrusters:", quat_thruster)
+q_mb1 = np.array([0.7071, 0.7071, 0, 0])  # w, x, y, z
+q_mb2 = np.array([0.7071, 0, 0.7071, 0])  # w, x, y, z
+quat_mb = multiply_quat(q_mb1, q_mb2)
+
+b_q_iy_z = np.array([0.7071, -0.7071, 0, 0])  # w, x, y, z
+# b_q_z_t = np.array([-0.354, 0.146, -0.354, 0.854])  # w, x, y, z
+b_q_z_t = np.array([1, 0, -0, 0.])  # w, x, y, z
+b_q = multiply_quat(b_q_iy_z, b_q_z_t)
+m_q = multiply_quat(quat_mb, b_q)
+
+print("Calculated quaternion for sub thrusters:", m_q)
